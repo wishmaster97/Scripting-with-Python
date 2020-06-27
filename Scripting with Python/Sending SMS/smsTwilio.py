37ba09c5df8ed011c77450eff50c35e7ecf03427
wishmaster97 <t.abhijeet97@gmail.com>
2020-06-28 00:01:50 +0530

    auth token removed

--- Scripting with Python/Sending SMS/smsTwilio.py
+++ Scripting with Python/Sending SMS/smsTwilio.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
-account_sid = 'AC2415d39d48b279d75c389a65564b753a' 
-auth_token = 'd3a6c4b6711e25be80f053d3f7594f08' 
+account_sid = '<sid>' 
+auth_token = '<auth_token>' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
